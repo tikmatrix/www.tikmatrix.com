f17e906adae28ca097470ee8a9d47c0aa06a0d07
niostack <niostack@gmail.com>
2024-05-25 05:18:00 +0800

    aos

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import HomepageHeader from '@site/src/components/HomepageHeader';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import PricingPage from '@site/src/components/PricingPage';
 import FrequentlyAsked from '@site/src/components/FrequentlyAsked';
-
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import 'boxicons/css/boxicons.min.css';
 import './index.css'
 
@@ -11,6 +13,11 @@ import './index.css'
 
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // 动画持续时间，单位毫秒
+    });
+  }, []);
 
   return (
     <Layout>
