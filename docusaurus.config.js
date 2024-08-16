fa4eb938cbfa83a041548fc672fe4cbf1af9a457
niostack <niostack@gmail.com>
2024-08-16 19:32:57 +0800

    1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://www.tikmatrix.com/',
+  url: 'https://tikmatrix.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
