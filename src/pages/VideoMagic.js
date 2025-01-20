baad57e577a95c1a3d6d9feedbbbca11c9b6508e
niostack <niostack@gmail.com>
2025-01-20 22:00:35 +0800

    更新VideoMagic和首页组件以添加结构化数据支持

--- src/pages/VideoMagic.js
+++ src/pages/VideoMagic.js
@@ -9,15 +9,62 @@ import 'aos/dist/aos.css';
 import 'boxicons/css/boxicons.min.css';
 import './VideoMagic.css'
 import Translate, { translate } from '@docusaurus/Translate';
+import Head from '@docusaurus/Head';
 export default function VideoMagic() {
   useEffect(() => {
     AOS.init({
       duration: 1000, // 动画持续时间，单位毫秒
     });
   }, []);
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "SoftwareApplication",
+    "name": "VideoMagic",
+    "url": "https://tikmatrix.com/videomagic",
+    "applicationCategory": "Social Media Management",
+    "operatingSystem": "Windows",
+    "screenshot": [
+      "https://tikmatrix.com/img/video-magic-startup1.png",
+      "https://tikmatrix.com/img/video-magic-startup2.png",
+    ],
 
+    "offers": {
+      "@type": "Offer",
+      "priceCurrency": "USDT",
+      "price": "0",
+      "description": translate({ message: 'Free' }),
+    },
+    "description": translate({ message: 'Video Magic is a professional video generation software designed for operators. It can generate unique videos for you, greatly enhancing operational efficiency' }),
+    "featureList": [
+      translate({ message: 'Video download' }),
+      translate({ message: 'Video generate' }),
+      translate({ message: 'Recognize subtitles' }),
+      translate({ message: 'Translate subtitles' }),
+    ],
+    "softwareVersion": "1.0.0",
+    "downloadUrl": "https://api.tikmatrix.com/download-videomagic",
+    "supportUrl": "https://t.me/+iGhozoBfAbI5YmE1",
+    "aggregateRating": {
+      "@type": "AggregateRating",
+      "ratingValue": "4.9",
+      "ratingCount": "1000000",
+      "bestRating": "5",
+      "worstRating": "1",
+    },
+    "author": {
+      "@type": "Organization",
+      "name": "TikMatrix",
+      "url": "https://tikmatrix.com",
+      "logo": "https://tikmatrix.com/img/logo.png",
+    },
+  }
   return (
     <Layout title={translate({ message: 'VideoMagic' })} description="Video Magic is a professional video generation software designed for operators. It can generate unique videos for you, greatly enhancing operational efficiency">
+      <Head>
+        <script type="application/ld+json">
+          {JSON.stringify(jsonLd)}
+        </script>
+      </Head>
       <VideoMagicHeader />
       <main>
         <VideoMagicFeatures />
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,19 +9,71 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import 'boxicons/css/boxicons.min.css';
 import './index.css'
-
-
+import Translate, { translate } from '@docusaurus/Translate';
+import Head from '@docusaurus/Head';
 
 
 export default function Home() {
+
   useEffect(() => {
     AOS.init({
       duration: 1000, // 动画持续时间，单位毫秒
     });
   }, []);
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "SoftwareApplication",
+    "name": "TikMatrix",
+    "url": "https://tikmatrix.com",
+    "applicationCategory": "Social Media Management",
+    "operatingSystem": "Windows",
+    "screenshot": [
+      "https://tikmatrix.com/img/startup1.png",
+      "https://tikmatrix.com/img/startup2.png",
+      "https://tikmatrix.com/img/startup3.png",
+    ],
 
+    "offers": {
+      "@type": "Offer",
+      "priceCurrency": "USDT",
+      "price": "0",
+      "description": translate({ message: 'Free' }),
+    },
+    "description": translate({ message: 'TikMatrix is a professional TikTok matrix software designed for operators. It controls multiple Android phones via a computer for automatic registration, login, account nurturing, messaging, and posting, greatly enhancing operational efficiency' }),
+    "featureList": [
+      translate({ message: 'Phone screen mirroring to a computer' }),
+      translate({ message: 'Automatic registration' }),
+      translate({ message: 'Automatic login' }),
+      translate({ message: 'Automatic account nurturing' }),
+      translate({ message: 'Automatic account nurturing' }),
+      translate({ message: 'Automatic posting' }),
+      translate({ message: 'Automatic following' }),
+      translate({ message: 'Automatic commenting' }),
+    ],
+    "softwareVersion": "1.9.5",
+    "downloadUrl": "https://api.tikmatrix.com/download",
+    "supportUrl": "https://t.me/+iGhozoBfAbI5YmE1",
+    "aggregateRating": {
+      "@type": "AggregateRating",
+      "ratingValue": "4.9",
+      "ratingCount": "1000000",
+      "bestRating": "5",
+      "worstRating": "1",
+    },
+    "author": {
+      "@type": "Organization",
+      "name": "TikMatrix",
+      "url": "https://tikmatrix.com",
+      "logo": "https://tikmatrix.com/img/logo.png",
+    },
+  }
   return (
     <Layout>
+      <Head>
+        <script type="application/ld+json">
+          {JSON.stringify(jsonLd)}
+        </script>
+      </Head>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
