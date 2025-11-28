b8a3a8f81146f8e350b2cd85d5cf5722f3f72da2
niostack <niostack@gmail.com>
2025-11-28 10:36:30 +0800

    移除 Tawk.to 脚本，简化主页组件结构

--- src/pages/index.js
+++ src/pages/index.js
@@ -97,24 +97,7 @@ export default function Home() {
         {/* <FrequentlyAsked /> */}
         <ContactPage />
         <Testimonials />
-        {/* Start of Tawk.to Script */}
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              (function(){
-              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/683bc34961be34190fe14eed/1iskn5mjr';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-              })();
-            `
-          }}
-        />
-        {/* End of Tawk.to Script */}
+
       </main>
     </Layout>
   );
